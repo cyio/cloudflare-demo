c834c563a53c987ef7d037c98cb361cf902ea444
Oaker <icaner@qq.com>
2023-05-08 11:59:00 +0800

    增删改查

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS Comments;
 CREATE TABLE Comments(
   id          TEXT NOT NULL PRIMARY KEY,
   createdAt   TEXT NOT NULL,
+  updatedAt   TEXT,
   content     TEXT NOT NULL,
   nickname    TEXT NOT NULL
 );
-INSERT INTO Comments(id,createdAt,content,nickname) VALUES ('989c9329-e989-46a7-87ab-dee178aa417a','2022-12-10T12:37:40.106Z','first!','test');
+INSERT INTO Comments(id,createdAt,updatedAt,content,nickname) VALUES ('989c9329-e989-46a7-87ab-dee178aa417a','2022-12-10T12:37:40.106Z','2022-12-10T12:37:40.106Z','first!','test');
--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,11 @@ interface PostCommentBody {
 	content: string;
 	nickname: string;
 }
-
+interface UpdateCommentBody {
+	id: string;
+	content: string;
+}
+// 查询所有评论
 const getComments = async (env: Env, page: number, pageSize: number) => {
 	const { results } = await env.DB.prepare(
 		'SELECT * FROM Comments ORDER BY rowid DESC LIMIT ? OFFSET ?'
@@ -45,6 +49,30 @@ const getComments = async (env: Env, page: number, pageSize: number) => {
 	const res = await env.DB.prepare(
 		`SELECT COUNT(*) as count FROM Comments`
 	).first<{ count: number }>();
+
+	return Response.json({
+		data: results,
+		total: res.count,
+	});
+};
+
+// 按昵称查询评论
+// http://127.0.0.1:8787/api/comment?nickName=jack
+// npx wrangler d1 execute demo_db --local --command="SELECT * FROM Comments WHERE nickname='jack'"
+const getComment = async (env: Env, page: number, pageSize: number, nickName: string) => {
+	console.log('debug', nickName)
+	const condition = 'WHERE nickname = ?'
+	const { results } = await env.DB.prepare(
+		'SELECT * FROM Comments WHERE nickname = ? ORDER BY rowid DESC LIMIT ? OFFSET ?'
+	)
+		.bind(nickName, pageSize, (page - 1) * pageSize)
+		.all<Comment>();
+
+	const res = await env.DB.prepare(
+		`SELECT COUNT(*) as count FROM Comments WHERE nickname = ?`
+	)
+		.bind(nickName)
+		.first<{ count: number }>();
 
 	return Response.json({
 		data: results,
@@ -65,6 +93,26 @@ const postComment = async (env: Env, content: string, nickname: string) => {
 	return Response.json({ message: 'ok' });
 };
 
+// npx wrangler d1 execute demo_db --local --command="UPDATE Comments SET content = 'new1', updatedAt = '2023' WHERE id = '5c8e6d8f-338b-45f9-a859-9999740df6ec'"
+const updateComment = async (env: Env, id: string, content: string) => {
+	const updatedAt = new Date().toISOString();
+
+	await env.DB.prepare(
+		`UPDATE Comments SET content = ?, updatedAt = ? WHERE id = ?`
+	)
+		.bind(content, updatedAt, id)
+		.run();
+
+	return Response.json({ message: 'ok' });
+};
+
+const deleteComment = async (env: Env, id: string) => {
+	await env.DB.prepare(`DELETE FROM Comments WHERE id = ?`)
+		.bind(id)
+		.run();
+	return Response.json({ message: 'ok' });
+};
+
 export default {
 	async fetch(
 		request: Request,
@@ -77,25 +125,42 @@ export default {
 			return new Response("Hello World!");
 		}
 
-		if (request.method === 'GET' && pathname === '/api/comments') {
-			const page = Number(searchParams.get('page')) || 1;
-			const pageSize = Number(searchParams.get('pageSize')) || 10;
-			return await getComments(env, page, pageSize);
-		} else if (request.method === 'POST' && pathname === '/api/comments') {
-			const body = await request.json<PostCommentBody>();
-			const { content, nickname } = body;
-
-			// 一些简单的校验
-			if (
-				!content ||
-				!nickname ||
-				content.length > 255 ||
-				nickname.length > 100
-			) {
-				return new Response(null, { status: 400 });
+		if (request.method === 'GET') {
+			if (pathname === '/api/comment') {
+				const page = Number(searchParams.get('page')) || 1;
+				const pageSize = Number(searchParams.get('pageSize')) || 10;
+				const nickName = searchParams.get('nickName') || '';
+				return await getComment(env, page, pageSize, nickName);
+			} else if (pathname === '/api/comments') {
+				const page = Number(searchParams.get('page')) || 1;
+				const pageSize = Number(searchParams.get('pageSize')) || 10;
+				return await getComments(env, page, pageSize);
 			}
-
-			return await postComment(env, body.content, body.nickname);
+		} else if (request.method === 'POST') {
+			if (pathname === '/api/comment/add') {
+				const body = await request.json<PostCommentBody>();
+				const { content, nickname } = body;
+	
+				// 一些简单的校验
+				if (
+					!content ||
+					!nickname ||
+					content.length > 255 ||
+					nickname.length > 100
+				) {
+					return new Response(null, { status: 400 });
+				}
+	
+				return await postComment(env, body.content, body.nickname);
+			} else if (pathname === '/api/comment/update') {
+				const body = await request.json<UpdateCommentBody>();
+				const { content, id } = body;
+				return await updateComment(env, id, content);
+			} else if (pathname === '/api/comment/delete') {
+				const body = await request.json<UpdateCommentBody>();
+				const { content, id } = body;
+				return await deleteComment(env, id);
+			}
 		}
 
 		return new Response(null, { status: 404 });;
